7d8d71f868143a1f8eb1b27093b162f945bdf64e
IGNATIUS LIYAN <ignatiusliyan@gmail.com>
2024-07-16 01:01:37 +0530

    Add splash screen
    -Implement a splash screen with the fade-in logo transition
    
    Integrate the API in the application

--- lib/Screans/Internship Page.dart
+++ lib/Screans/Internship Page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:internshala_search_page/main.dart';
 
 class internship_page extends StatefulWidget {
   const internship_page({super.key});
@@ -8,8 +11,80 @@ class internship_page extends StatefulWidget {
 }
 
 class _internship_pageState extends State<internship_page> {
+
+
+  //Method to parse the data into json after checking the request has been succeeded
+
+  Future fetchdata() async{
+    final response = await http.get(Uri.parse('https://internshala.com/flutter_hiring/search'));
+
+    if (response.statusCode==200){
+      return json.decode(response.body);
+    }
+    else{
+      throw Exception('Failed to Load Data');
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar:AppBar(
+        leading: IconButton(
+          onPressed: (){},
+         icon: Icon(Icons.menu),
+        ),
+        title: Text('Internships'),
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon:Icon(Icons.search)
+          ),
+          IconButton(
+              onPressed: (){},
+              icon:Icon(Icons.bookmark_border)
+          ),
+          IconButton(
+              onPressed: (){},
+              icon:Icon(Icons.notifications_none)
+          ),
+          IconButton(
+              onPressed: (){},
+              icon:Icon(Icons.message_outlined)
+          ),
+        ],
+      ),
+      body:FutureBuilder(future: fetchdata(), builder: (context,snapshot){
+        if (snapshot.connectionState==ConnectionState.waiting){
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+                Center(
+                  child: CircularProgressIndicator(color: AppColors.primaryColor,),
+                )
+              ]);
+        }else if(snapshot.hasError){
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Icon(Icons.cloud_off,size: 50,color: AppColors.primaryColor,),
+              ),
+              Text('No Internet Connection')
+            ],
+          );
+        }
+        else{
+          return ListView.builder(
+            itemCount:snapshot.data['internship_ids'].length,
+            itemBuilder: (BuildContext context,int index){
+            return ListTile(
+              leading: Text(snapshot.data['internship_ids'][index].toString()),
+            );
+          },);
+        }
+      })
+    );
   }
 }
